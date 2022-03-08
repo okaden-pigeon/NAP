b629561784e4e1c9a1c96ae2e160779f16e046c5
natsukicandy <natsukicandy0727@gmail.com>
2022-03-08 08:26:59 +0000

    Tue, Mar 8, 2022, 8:26 AM +00:00

--- myproject/myproject/base/views.py
+++ myproject/myproject/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-# Create your views here.
 def top(request):
-    return HttpResponse('Hello World')
+    template = loader.get_template('base/top.html')
+    ctx = {'title': 'Django学習ちゃんねる(仮)'}
+    return HttpResponse(template.render(ctx, request))
