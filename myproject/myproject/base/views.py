5a63c9fcf1f8aea037bf392b815de1708d79af29
natsukicandy <natsukicandy0727@gmail.com>
2022-03-08 08:35:11 +0000

    Tue, Mar 8, 2022, 8:35 AM +00:00

--- myproject/myproject/base/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-def top(request):
-    template = loader.get_template('base/top.html')
-    ctx = {'title': 'Django学習ちゃんねる(仮)'}
-    return HttpResponse(template.render(ctx, request))
