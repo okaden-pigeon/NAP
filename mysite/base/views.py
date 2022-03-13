0a300b0aee8ba212b4c1447e8009b3a9ff7896ed
natsukicandy <93819696+natsukicandy@users.noreply.github.com>
2022-03-13 06:05:20 +0000

    Sun, Mar 13, 2022, 6:05 AM +00:00

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+def top(request):
+    return HttpResponse('Hello World')
