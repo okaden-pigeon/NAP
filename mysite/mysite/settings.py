d51e8e847aecdf1ca4b544ff3eb5bc77d41879b0
natsukicandy <93819696+natsukicandy@users.noreply.github.com>
2022-03-13 06:08:42 +0000

    Sun, Mar 13, 2022, 6:08 AM +00:00

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+
     'base',
+    'DIRS': [os.path.join(BASE_DIR, 'templates')],
 ]
 
 MIDDLEWARE = [
