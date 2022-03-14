ea831012f253b5f8fc465e07193090405e6f8990
entiku <94211977+entiku@users.noreply.github.com>
2022-03-14 12:48:05 +0000

    Mon, Mar 14, 2022, 12:48 PM +00:00

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.urls')),
+    path('accounts/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')), # django学習帳 3-1
 
 ]
