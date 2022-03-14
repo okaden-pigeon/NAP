f27f15be09d88b42320ac8ac54fc38f4d2fb9739
natsukicandy <93819696+natsukicandy@users.noreply.github.com>
2022-03-14 11:18:42 +0000

    Mon, Mar 14, 2022, 11:18 AM +00:00

--- mysite/apps/urls.py
+++ mysite/apps/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('myhistory/', views.MyhistoryView.as_view(), name="myhistory"),
     path('mylist/', views.MylistView.as_view(), name="mylist"),
-    path('product-create/', views.ProductCreateView.as_view(), name="product_create"),
-    path('product-recreate/', views.ProductRecreateView.as_view(), name="product_recreate"),
+    path('product_create/', views.ProductCreateView.as_view(), name="product_create"),
+    path('product_recreate/', views.ProductRecreateView.as_view(), name="product_recreate"),
     path('product/', views.ProductView.as_view(), name="product"),
-    path('useredit/', views.UserEditView.as_view(), name="user_edit"),
+    path('user_edit/', views.UserEditView.as_view(), name="user_edit"),
 ]
