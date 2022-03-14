476e621f635333cb96a8d0936a82bbe3c93cf564
natsukicandy <93819696+natsukicandy@users.noreply.github.com>
2022-03-14 09:32:18 +0000

    Mon, Mar 14, 2022, 9:32 AM +00:00

--- mysite/apps/views.py
+++ mysite/apps/views.py
@@ -7,8 +7,11 @@ from django.views import generic
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
-class ProductView(generic.TemplateView):
-    template_name = "product.html"
+class MyhistoryView(generic.TemplateView):
+    template_name = "myhistory.html"
+
+class MylistView(generic.TemplateView):
+    template_name = "mylist.html"
 
 class ProductCreateView(generic.TemplateView):
     template_name = "product_create.html"
@@ -16,17 +19,8 @@ class ProductCreateView(generic.TemplateView):
 class ProductRecreateView(generic.TemplateView):
     template_name = "product_recreate.html"
 
-class MylistView(generic.TemplateView):
-    template_name = "mylist.html"
-
-class MyhistoryView(generic.TemplateView):
-    template_name = "myhistory.html"
+class ProductView(generic.TemplateView):
+    template_name = "product.html"
 
 class UserEditView(generic.TemplateView):
     template_name = "user_edit.html"
-
-class MyhistoryView(generic.TemplateView):
-    template_name = "myhistory.html"
-
-class MypageView(generic.TemplateView):
-    template_name = "mypage.html"
