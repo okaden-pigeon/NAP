d246ed5a6cb3655d1ae5777654642e4a0a01e9c9
natsukicandy <93819696+natsukicandy@users.noreply.github.com>
2022-03-14 09:36:30 +0000

    Mon, Mar 14, 2022, 9:36 AM +00:00

--- mysite/apps/urls.py
+++ mysite/apps/urls.py
@@ -5,11 +5,12 @@ from . import views
 app_name = 'apps'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('product/', views.ProductView.as_view(), name="product"),
+    path('myhistory/', views.MyhistoryView.as_view(), name="myhistory"),
+    path('mylist/', views.MylistView.as_view(), name="mylist"),
     path('product-create/', views.ProductCreateView.as_view(), name="product_create"),
     path('product-recreate/', views.ProductRecreateView.as_view(), name="product_recreate"),
-    path('mylist/', views.MylistView.as_view(), name="mylist"),
-    path('myhistory/', views.MyhistoryView.as_view(), name="myhistory"),
-    path('mypage/', views.MypageView.as_view(), name="mypage"),
+    path('product/', views.ProductView.as_view(), name="product"),
+    path('useredit/', views.UserEditView.as_view(), name="user_edit"),
+
     path('accounts/', include('accounts.urls')), # django学習帳 3-1
 ]
