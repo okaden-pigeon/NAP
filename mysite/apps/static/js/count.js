ba8324a57373dc4800a18d3d69c3705572dcd1dd
natsukicandy <93819696+natsukicandy@users.noreply.github.com>
2022-03-15 06:02:37 +0000

    Tue, Mar 15, 2022, 6:02 AM +00:00

--- mysite/apps/static/js/count.js
+++ mysite/apps/static/js/count.js
@@ -1,4 +1,9 @@
-function viewStrLen(){
+function product_name_str_len(){
+  var len = document.getElementById("area1").value.length;
+  document.getElementById("strLen").innerText = len + "/500文字";
+}
+
+function product_explanation_str_len(){
   var len = document.getElementById("area1").value.length;
   document.getElementById("strLen").innerText = len + "/500文字";
 }
