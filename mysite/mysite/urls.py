9312beba64c5ba302d6b569545d00ed683ba630d
entiku <94211977+entiku@users.noreply.github.com>
2022-03-16 12:52:27 +0000

    Wed, Mar 16, 2022, 12:52 PM +00:00

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.urls')),
     path('accounts/', include('allauth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), # django学習帳 3-1
     path('accounts/', include('accounts.urls')), # django学習帳 3-1
 
 ]
