37a1523ccc2facc92391e0d097db77fe07bddcd6
natsukicandy <93819696+natsukicandy@users.noreply.github.com>
2022-03-16 05:00:52 +0000

    Wed, Mar 16, 2022, 5:00 AM +00:00

--- mysite/apps/static/js/product_image_slide.js
+++ mysite/apps/static/js/product_image_slide.js
@@ -1,3 +1,22 @@
-var img = document.getElementById(“image_file”);
+var pics_src = new Array("pics/1.jpg","pics/2.jpg","pics/3.jpg"); //　ユーザが登録した写真に切り替える
+var num = 0;
 
-img.src = “img/image1.png”;　// ユーザが登録した写真
+function go_forward(){
+    if (num == 2) {
+        num = 0;
+    }
+    else {
+        num ++;
+    }
+    document.getElementById("mypic").src=pics_src[num];
+}
+
+function go_back(){
+    if (num == 0) {
+        num = 2;
+    }
+    else {
+        num --;
+    }
+    document.getElementById("mypic").src=pics_src[num];
+}
