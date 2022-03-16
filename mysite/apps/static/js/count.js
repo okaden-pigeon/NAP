29a8008067e3cecdfe96f013e058d05ec500b059
entiku <94211977+entiku@users.noreply.github.com>
2022-03-16 11:57:06 +0000

    Wed, Mar 16, 2022, 11:57 AM +00:00

--- mysite/apps/static/js/count.js
+++ mysite/apps/static/js/count.js
@@ -7,3 +7,18 @@ function product_explanation_str_len(){
   var len = document.getElementById("area2").value.length;
   document.getElementById("strLen").innerText = len + "/500文字";
 }
+
+function nickname_str_len(){
+  var len = document.getElementById("area3").value.length;
+  document.getElementById("nickname_str_len").innerText = len + "/20文字";
+}
+
+function password_str_len(){
+  var len = document.getElementById("area4").value.length;
+  document.getElementById("password_str_len").innerText = len + "文字";
+}
+
+function password_confirmation_str_len(){
+  var len = document.getElementById("area5").value.length;
+  document.getElementById("password_confirmation_str_len").innerText = len + "文字";
+}
