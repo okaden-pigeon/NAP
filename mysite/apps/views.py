a48de7c5144314f6c5973ec3b52081bce6d1c5eb
entiku <94211977+entiku@users.noreply.github.com>
2022-03-19 02:35:08 +0000

    Sat, Mar 19, 2022, 2:35 AM +00:00

--- mysite/apps/views.py
+++ mysite/apps/views.py
@@ -15,6 +15,11 @@ class IndexView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context["item"] = items
         return context
+    def get_context_data(self,**kwargs):
+        items = Genres.objects.all()
+        context = super().get_context_data(**kwargs)
+        context["item"] = items
+        return context
 
 class LoginView(LoginView):
     template_name = "login.html"
