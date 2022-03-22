c30002485904b89dfd0d747575ebe43cf8ec566a
entiku <94211977+entiku@users.noreply.github.com>
2022-03-22 08:39:39 +0000

    Tue, Mar 22, 2022, 8:39 AM +00:00

--- mysite/apps/static/js/confirm.js
+++ mysite/apps/static/js/confirm.js
@@ -2,8 +2,8 @@ function CheckEmail_1() {
     //IE対応の為変更
     //var mail = email_1.value; //メールフォームの値を取得
     //var mailConfirm = emailConfirm_1.value; //メール確認用フォームの値を取得
-    var mail = document.getElementById("email_1").value; //メールフォームの値を取得
-    var mailConfirm = document.getElementById("emailConfirm_1").value; //メール確認用フォームの値を取得
+    var mail = document.getElementById("mail").value; //メールフォームの値を取得
+    var mailConfirm = document.getElementById("mail_confirm").value; //メール確認用フォームの値を取得
     // パスワードの一致確認
     if (mail != mailConfirm){
       alert("パスワードと確認用パスワードが一致しません"); // 一致していなかったら、エラーメッセージを表示する
