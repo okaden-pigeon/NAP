0de825fa5d50beec50de1141f9355254924cdb21
kaheiseki <kaheiseki.code@gmail.com>
2022-03-23 01:46:45 +0900

    ルーティングの変更

--- mysite/apps/forms.py
+++ mysite/apps/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
+from .models import Users
+
 class EditItemForm(forms.Form):
     item_name = forms.CharField(max_length=40)
+
+class UserInfo(forms.ModelForm):
+    class Meta:
+        model = Users
+        fields = ("user_name","user_pass","icon")
