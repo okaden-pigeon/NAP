221fe99988e28fdd451d0b9c0c73e3b6ba04db90
entiku <94211977+entiku@users.noreply.github.com>
2022-03-22 09:13:31 +0000

    Tue, Mar 22, 2022, 9:13 AM +00:00

--- mysite/apps/static/js/confirm.js
+++ mysite/apps/static/js/confirm.js
@@ -2,8 +2,8 @@ function CheckEmail_1() {
     //IE対応の為変更
     //var mail = email_1.value; //メールフォームの値を取得
     //var mailConfirm = emailConfirm_1.value; //メール確認用フォームの値を取得
-    var mail = document.getElementById("mail").value; //メールフォームの値を取得
-    var mailConfirm = document.getElementById("mail_confirm").value; //メール確認用フォームの値を取得
+    var mail = document.getElementById("mail1").value; //メールフォームの値を取得
+    var mailConfirm = document.getElementById("mail1_confirm").value; //メール確認用フォームの値を取得
     // パスワードの一致確認
     if (mail != mailConfirm){
       alert("パスワードと確認用パスワードが一致しません"); // 一致していなかったら、エラーメッセージを表示する
@@ -14,8 +14,20 @@ function CheckEmail_1() {
   };
 
   function CheckPassword() {
-    var password = document.getElementById("mail").value; //メールフォームの値を取得
-    var password_confirm = document.getElementById("mail_confirm").value; //メール確認用フォームの値を取得
+    var password = document.getElementById("inputPassword1").value; //メールフォームの値を取得
+    var password_confirm = document.getElementById("inputPassword1_confirm").value; //メール確認用フォームの値を取得
+    // パスワードの一致確認
+    if (password != password_confirm){
+      alert("パスワードと確認用パスワードが一致しません"); // 一致していなかったら、エラーメッセージを表示する
+      return false;
+    }else{
+      return true;
+    }
+  };
+
+  function CheckPassword() {
+    var password = document.getElementById("inputPassword2").value; //メールフォームの値を取得
+    var password_confirm = document.getElementById("inputPassword2_confirm").value; //メール確認用フォームの値を取得
     // パスワードの一致確認
     if (password != password_confirm){
       alert("パスワードと確認用パスワードが一致しません"); // 一致していなかったら、エラーメッセージを表示する
