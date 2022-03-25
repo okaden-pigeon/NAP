9c11e29ec0803b7f5768325ac23e2d0b700f7524
kaheiseki <kaheiseki.code@gmail.com>
2022-03-26 00:52:27 +0900

    urls.pyのリファクタリング

--- mysite/apps/urls.py
+++ mysite/apps/urls.py
@@ -1,27 +1,37 @@
 from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib.staticfiles.urls import static 
-import sys
-sys.path.append("../")
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns,static 
 
 from . import views
 import mysite.settings
 
+import sys
+sys.path.append("../")
+
+
 app_name = 'apps'
 urlpatterns = [
-    path('index', views.IndexView.as_view(), name="index"),
+    # ログインページ
     path('', views.LoginView.as_view(), name="login"),
+    # ログアウト
+    path('logout/', views.Logout.as_view(), name='logout'),
+    # 大学認証ページ
+    path('university_register/', views.UniversityRegisterView.as_view(), name="university_register"),
+    # ユーザー登録ページ
+    path("user_register/",views.SignUpView.as_view(),name="signup"),
+    # 商品一覧ページ
+    path('index', views.IndexView.as_view(), name="index"),
+    # 商品登録ページ
+    path('product_create/', views.ProductCreateView.as_view(), name="product_create"),
+
+# ------------------------以下未実装のルーティング------------------------------
     # path('myhistory/', views.MyhistoryView.as_view(), name="myhistory"),
     # path('mylist/', views.MylistView.as_view(), name="mylist"),
-    path('product_create/', views.ProductCreateView.as_view(), name="product_create"),
     # path('product_recreate/', views.ProductRecreateView.as_view(), name="product_recreate"),
     # path('product/', views.ProductView.as_view(), name="product"),
-    path('university_register/', views.UniversityRegisterView.as_view(), name="university_register"),
     # path('user_edit/', views.UserEditView.as_view(), name="user_edit"),
-    # path('user_register/', views.UserRegisterView.as_view(), name="user_register"),
-    path("user_register/",views.SignUpView.as_view(),name="signup"),
-    path('logout/', views.Logout.as_view(), name='logout'),
+# ------------------------ルーティングここまで-----------------------------------
     ]
 
+# 画像をstaticから利用するための設定
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(mysite.settings.MEDIA_URL,document_root=mysite.settings.MEDIA_ROOT)
