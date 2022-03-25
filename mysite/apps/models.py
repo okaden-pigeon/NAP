9a89c0a9fc8bd3a72ea14d0c473c1913fe66456f
kaheiseki <kaheiseki.code@gmail.com>
2022-03-25 22:10:16 +0900

    dbの修正

--- mysite/apps/models.py
+++ mysite/apps/models.py
@@ -71,39 +71,39 @@ class Images(models.Model):
   class Meta:
     db_table = "Images"
 
-class Check(models.Model):
-  image = models.ImageField(upload_to = "./images/")
-  first = models.CharField(max_length = 20,default="")
-  last = models.CharField(max_length = 20,default = "")
-  def __str__(self):
-    return self.image
-  class Meta:
-    db_table = "Check"
+# class Check(models.Model):
+#   image = models.ImageField(upload_to = "./images/")
+#   first = models.CharField(max_length = 20,default="")
+#   last = models.CharField(max_length = 20,default = "")
+#   def __str__(self):
+#     return self.image
+#   class Meta:
+#     db_table = "Check"
 
 # 商品情報と分類をつなぐためのテーブル
-class ItemClassification(models.Model):
-  item_id = models.IntegerField()
-  genre_id = models.IntegerField()
-  class Meta:
-    db_table = "ItemClasification"
+# class ItemClassification(models.Model):
+#   item_id = models.IntegerField()
+#   genre_id = models.IntegerField()
+#   class Meta:
+#     db_table = "ItemClasification"
 
 # 商品情報と画像をつなぐためのテーブル
-class ItemImage(models.Model):
-  item_id = models.IntegerField()
-  image_id = models.IntegerField()
-  class Meta:
-    db_table = "ItemImage"
+# class ItemImage(models.Model):
+#   item_id = models.IntegerField()
+#   image_id = models.IntegerField()
+#   class Meta:
+#     db_table = "ItemImage"
 
 # ユーザー情報と商品情報をつなぐためのテーブル
-class UserListing(models.Model):
-  user_id = models.IntegerField()
-  item_id = models.IntegerField()
-  class Meta:
-    db_table = "UserListing"
+# class UserListing(models.Model):
+#   user_id = models.IntegerField()
+#   item_id = models.IntegerField()
+#   class Meta:
+#     db_table = "UserListing"
 
 # ユーザー情報と商品のお気に入りをつなぐためのテーブル
-class Likes(models.Model):
-  user_id = models.IntegerField()
-  item_id = models.IntegerField()
-  class Meta:
-    db_table = "Likes"
+# class Likes(models.Model):
+#   user_id = models.IntegerField()
+#   item_id = models.IntegerField()
+#   class Meta:
+#     db_table = "Likes"
